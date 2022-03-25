a13cd71e6e8b90dee516e7dbc6f87bc80de8925a
malikmukhtar <malikmukhtar63@gmail.com>
2022-03-25 11:53:14 +0100

    create course form

--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -1,12 +1,50 @@
-import axios from 'axios'
-import InstructorRoute from '../../../components/routes/InstructorRoute'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Select, Button } from "antd";
+import InstructorRoute from "../../../components/routes/InstructorRoute";
+import { SaveOutlined } from "@ant-design/icons";
+import CourseCreateForm from "../../../components/forms/CourseCreateForm";
 
-const CreateCourse = () =>{
-    return (
-        <InstructorRoute>
-        <h1 className='jumbotron text-center square'>Create Course</h1>
-        </InstructorRoute>
-    )
-}
+const { Option } = Select;
 
-export default CreateCourse
+const CreateCourse = () => {
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: "9.99",
+    uploading: false,
+    paid: true,
+    category:'',
+    loading: false,
+    imagePreview: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleImage = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
+  return (
+    <InstructorRoute>
+      <h1 className="jumbotron text-center square">Create Course</h1>
+      <div className="pt-3 pb-3">
+        <CourseCreateForm
+          handleSubmit={handleSubmit}
+          handleImage={handleImage}
+          handleChange={handleChange}
+          values={values}
+          setValues={setValues}
+        />
+      </div>
+      <pre>{JSON.stringify(values, null, 4)}</pre>
+    </InstructorRoute>
+  );
+};
+
+export default CreateCourse;
